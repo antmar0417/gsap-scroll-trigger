f4886757cbc63b94e5b53793e2ea31d013f3f5d9
antmar0417 <tonyboy@live.se>
2022-07-03 21:04:06 +0200

    added  text into the sections

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const doubleDownDiv = document.querySelector(".double-down");
+const doubleDownIcon = document.getElementById("down");
+
+doubleDownDiv.addEventListener("mouseover", () => {
+  doubleDownIcon.style.fontSize = "300%";
+});
+
+doubleDownDiv.addEventListener("mouseout", () => {
+  doubleDownIcon.style.fontSize = "100%";
+});
+
 let tl = gsap.timeline({
   scrollTrigger: {
     trigger: ".dark",
@@ -9,10 +20,3 @@ tl.from("img", { x: -200, opacity: 0, duration: 1.5 }, "+=0.5").from(
   { y: 300, opacity: 0, duration: 1.5 },
   "-=1"
 );
-
-function ingredientsHover() {
-  document.getElementById("down").style.fontSize = "300%";
-}
-function ingredientsNormal() {
-  document.getElementById("down").style.fontSize = "100%";
-}
